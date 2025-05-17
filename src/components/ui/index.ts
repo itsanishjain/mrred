86401951d92586d472b9d92e1a2dc0afae1c35c6
itsanishjain <helloanishjain@gmail.com>
2025-05-17 21:09:05 +0530

    fixx

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,5 @@
 // Terminal UI Components
 export * from "./terminal-box";
-export * from "./terminal-button";
 export * from "./terminal-input";
 export * from "./terminal-card";
 export * from "./terminal-text";
